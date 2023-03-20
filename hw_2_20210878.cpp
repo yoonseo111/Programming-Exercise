9d2962c072b913ef4b0eac5730eb289f1497fa12
yoonseo111 <126842110+yoonseo111@users.noreply.github.com>
2023-03-20 20:15:24 +0900

    02

---
+++ hw_2_20210878.cpp
@@ -0,0 +1,20 @@
+// ±Ë¿±º≠
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter temperature: ";
+	int temp;
+	cin >> temp;
+
+	if (temp < 30)
+		cout << "too cold" << endl;
+	else if (temp > 100)
+		cout << "too hot" << endl;
+	else
+		cout << "just right" << endl;
+
+	return 0;
+}
